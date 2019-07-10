0f2adaebe420840451a8cb533a22f948d4e4cef6
Colin Gross <grosscol@gmail.com>
2019-07-10 10:22:51 -0400

    Make numer & denom name extractors for convenience   For use in bitstomach and pictoralist

--- R/spek_extractors.R
+++ R/spek_extractors.R
@@ -4,12 +4,30 @@
 #' @describeIn spek_extractors
 #' @note this function will be extracted to spektools package
 #' @export
-get_id_col_from_spek <- function(spek) {
-  column_list <- get_column_list(spek)
-  column_names <- sapply(column_list, FUN=get_name_of_column)
-  column_uses <- sapply(column_list, FUN=get_use_of_column)
+get_id_col_from_spek <- function(spek){ get_colnames_by_use(spek, "identifier") }
 
-  return(column_names[which(column_uses == "identifier")])
+#' @title Get Numerator Column Name
+#' @description Get name of column that has "numerator" column use in the column spec.
+#' @return name of single numerator column
+#' @describeIn spek_extractors
+#' @note this function will be extracted to spektools package.
+#' Does not handle multi-numerator data.
+#' @importFrom dplyr first
+#' @export
+get_numerator_colname <- function(spek){
+  dplyr::first(get_colnames_by_use(spek, "numerator"), default=c(NA))
+}
+
+#' @title Get Denominator Column Name
+#' @description Get name of column that has "denominator" column use in the column spec.
+#' @return name of single denominator column or NA when no denominator is found
+#' @describeIn spek_extractors
+#' @note this function will be extracted to spektools package
+#' Does not handle multi-denominator data.
+#' @importFrom dplyr first
+#' @export
+get_denominator_colname <- function(spek){
+  dplyr::first(get_colnames_by_use(spek, "denominator"), default=c(NA))
 }
 
 #' @title Get Value or Numerator columns
@@ -55,4 +73,19 @@ get_name_of_column <- function(column_specification) {
 #' @export
 get_use_of_column <- function(column_specification) {
   column_specification[['http://example.com/slowmo#ColumnUse']][[1]][['@value']]
+}
+
+#' @title Get Single Column By Use
+#' @description Extract column names from spek
+#' @param spek list representation of spek
+#' @param use string name of column use
+#' @return character vector of the column names
+#' @describeIn spek_extractors
+#' @note this function will be extracted to spektools package
+get_colnames_by_use <- function(spek, use){
+  column_list <- get_column_list(spek)
+  column_names <- sapply(column_list, FUN=get_name_of_column)
+  column_uses <- sapply(column_list, FUN=get_use_of_column)
+
+  return(column_names[which(column_uses == use)])
 }
--- tests/testthat/test_spek_extractors.R
+++ tests/testthat/test_spek_extractors.R
@@ -1,13 +1,35 @@
 context("testing spek extractors")
 
+# For convenience always use the same order: sham, mtx, va
 SHAM_SPEK <- read_spek(get_spek_path('sham'))
+MTX_SPEK <- read_spek(get_spek_path('mtx'))
+VA_SPEK <- read_spek(get_spek_path('va'))
+
+EXPECTED_USE_COLNAMES <- list(numerators=c(NA, 'high_dose_scripts', 'documented'),
+                          denominators=c(NA,'total_scripts','total'),
+                          values=c('performance',NA,NA),
+                          identities=c('performer','practice','sta6a'))
+
 
 test_that("id column name extractor returns character vector", {
-  result <- get_id_col_from_spek(SHAM_SPEK)
-  expect_identical(result, "performer")
+  all_speks <- list(SHAM_SPEK, MTX_SPEK, VA_SPEK)
+  results <- sapply(all_speks, FUN=get_id_col_from_spek)
+  expect_identical(results, EXPECTED_USE_COLNAMES$identities)
 })
 
 test_that("value column name extractor returns name of value column", {
   result <- get_value_or_numerator_col_from_spek(SHAM_SPEK)
   expect_identical(result, "performance")
+})
+
+test_that("numerator column name extractor returns name of numerator column",{
+  all_speks <- list(SHAM_SPEK, MTX_SPEK, VA_SPEK)
+  results <- sapply(all_speks, FUN=get_numerator_colname)
+  expect_identical(results, EXPECTED_USE_COLNAMES$numerators)
+})
+
+test_that("denominator column name extractor returns name of denominator column",{
+  all_speks <- list(SHAM_SPEK, MTX_SPEK, VA_SPEK)
+  results <- sapply(all_speks, FUN=get_denominator_colname)
+  expect_identical(results, EXPECTED_USE_COLNAMES$denominators)
 })

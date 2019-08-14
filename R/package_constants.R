d4334d1eefd3435ae9f88d363a940a7d06b8909d
Colin Gross <grosscol@gmail.com>
2019-08-14 11:05:46 -0400

    Continue R IRI constants consolidation   Include IRIs used in bitstomach and kb

--- R/package_constants.R
+++ R/package_constants.R
@@ -14,7 +14,7 @@ SE$COLUMN_USE_IRI   <- "http://example.com/slowmo#ColumnUse"
 SE$TABLE_SCHEMA_IRI <- "http://www.w3.org/ns/csvw#tableSchema"
 SE$COLUMN_NAME_IRI  <- "http://www.w3.org/ns/csvw#name"
 SE$COLUMN_DTYPE_IRI <- "http://www.w3.org/ns/csvw#datatype"
-SE$COLUMN_TITLE_IRI <- "http://www.w3.org/ns/csvw#titles"
+SE$COLUMN_TITLE_IRI <- "http://www.w3.org/ns/csvw#title"
 SE$DESCRIPTION_IRI  <- "http://purl.org/dc/terms/description"
 SE$TABLE_IRI        <- "http://www.w3.org/ns/csvw#Table"
 SE$COLUMNS_IRI      <- "http://www.w3.org/ns/csvw#columns"
@@ -25,3 +25,27 @@ SE$ABOUT_ORGANIZATION_IRI <- "http://example.com/slowmo#IsAboutOrganization"
 SE$ABOUT_MEASURE_IRI      <- "http://example.com/slowmo#IsAboutMeasure"
 SE$ABOUT_TEMPLATE_IRI     <- "http://example.com/slowmo#IsAboutTemplate"
 SE$HAS_CANDIDATE_IRI   <- "http://example.com/slowmo#HasCandidate"
+
+### Additional IRIs used in bitstomach ###
+SE$HAS_DISPOSITION_IRI <- "http://purl.obolibrary.org/obo/RO_0000091"
+SE$DEFAULT_APP_IRI <- "http://example.com/app#"
+SE$HAS_PERFORMERS_IRI <- "http://example.com/slowmo#IsAboutPerformer"
+SE$PERFORMER_IRI <- "http://purl.obolibrary.org/obo/psdo_0000085"
+
+# Slowmo ascribee IRIs
+SE$CAPABILITY_BARRIER_IRI <- "http://example.com/slowmo#CapabilityBarrier"
+SE$NEGATIVE_TREND_IRI     <- "http://purl.obolibrary.org/obo/psdo.owl/psdo_0000100"
+SE$POSITIVE_TREND_IRI     <- "http://purl.obolibrary.org/obo/psdo.owl/psdo_0000099"
+SE$NEGATIVE_GAP_IRI       <- "http://purl.obolibrary.org/obo/psdo.owl/psdo_0000105"
+SE$POSITIVE_GAP_IRI       <- "http://purl.obolibrary.org/obo/psdo.owl/psdo_0000104"
+SE$PERFORMANCE_GAP_IRI    <- "http://purl.obolibrary.org/obo/psdo.owl/psdo_0000106"
+SE$LARGE_GAP_IRI          <- "http://example.com/slowmo#LargeGap"
+
+# Additional preconditions from knowledge base
+SE$ACHEIVEMENT_IRI         <- "http://example.com/slowmo#Achievement"
+SE$CONSEC_NEG_GAP_IRI      <- "http://example.com/slowmo#ConsecutiveNegativePerformanceGapContent"
+SE$CONSEC_POS_GAP_IRI      <- "http://example.com/slowmo#ConsecutivePositivePerformanceGapContent"
+SE$GOAL_COMPARATOR_IRI     <- "http://purl.obolibrary.org/obo/psdo.owl/psdo_0000094"
+SE$SOCIAL_COMPARATOR_IRI   <- "http://purl.obolibrary.org/obo/psdo.owl/psdo_0000095"
+SE$STANDARD_COMPARATOR_IRI <- "http://purl.obolibrary.org/obo/psdo.owl/psdo_0000096"
+

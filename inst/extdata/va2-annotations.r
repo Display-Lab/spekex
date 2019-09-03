1257ef8a7b6ac1fd4a74ad2d4fd1aac42cbdc9ea
Colin Gross <grosscol@gmail.com>
2019-09-03 16:10:21 -0400

    Increase VA2 example annotation complexity.   Implement upward and downward trend examples.

--- inst/extdata/va2-annotations.r
+++ inst/extdata/va2-annotations.r
@@ -19,6 +19,23 @@ eval_positive_gap <- function(doc, ndoc){
   return(dplyr::last(rate) >= 0.9)
 }
 
+eval_positive_trend <- function(doc, ndoc){
+  raw_rate <- doc/(ndoc+doc)
+  rate <- ifelse(is.nan(raw_rate), 0, raw_rate)
+
+  tail_sorted <- !is.unsorted(utils::tail(rate,3), strictly=T)
+  is_tail_ascending <- !is.na(tail_sorted) && tail_sorted
+  return(is_tail_ascending)
+}
+
+eval_negative_trend <- function(doc, ndoc){
+  raw_rate <- doc/(ndoc+doc)
+  rate <- ifelse(is.nan(raw_rate), 0, raw_rate)
+
+  tail_rev_sorted <- !is.unsorted(rev(utils::tail(rate,3)), strictly=T)
+  is_tail_descending <- !is.na(tail_rev_sorted) && tail_rev_sorted
+  return(is_tail_descending)
+}
 
 # Annotation functions
 annotate_negative_gap <- function(data, spek){
@@ -31,4 +48,18 @@ annotate_positive_gap <- function(data, spek){
   data %>% group_by(id) %>%
     dplyr::filter(report_month == max(report_month)) %>%
     summarize(positive_gap = eval_positive_gap(documented, not_documented))
+}
+
+annotate_positive_trend <- function(data, spek){
+  data %>%
+    group_by(id) %>%
+    arrange(report_month) %>%
+    summarize(positive_trend = eval_positive_trend(documented, not_documented))
+}
+
+annotate_negative_trend <- function(data, spek){
+  data %>%
+    group_by(id) %>%
+    arrange(report_month) %>%
+    summarize(negative_trend = eval_negative_trend(documented, not_documented))
 }
